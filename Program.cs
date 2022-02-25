28da911223dddd2da7a07ac674920799bc8a70d4
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-02-24 18:55:57 -0600

    Documentation

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace DocumentMerger
     {
         static void Main(string[] args)
         {
+            // Variables
             string newFile;
             List<string> filenames = new List<string>();
-            int wordCount = 0;
+            int characterCount = 0;
             
-
+            // If 3 or more arguments given, run in non-interactive mode. 
             if(args.Length >= 3 ) {
                 newFile = args[0];
                 for(int i = 1; i < args.Length; i++) {
@@ -20,27 +21,37 @@ namespace DocumentMerger
                 }
                 try {
                     readToFile(newFile,filenames);
-                    wordCount = getCharacterCount(newFile);
-                    displayStats(newFile,wordCount);
+                    characterCount = getCharacterCount(newFile);
+                    displaySaveMessage(newFile,characterCount);
                 }
                 catch(Exception ex) {
                     displayException(ex);
                 }
             }
+
             // Display “Document Merger” followed by a blank line.
             welcomeUser();
 
             while(true) {               
-                int numFiles = 2;   // TODO: Dynamically prompt for files list            
+                int numFiles = 0;   
 
                 // Prompt the user for the name of the first text file.
                 // Verify that the first file exists. If not, give the user feedback and let them re-enter the first filename.
                 // Prompt the user for the name of the second document.
                 // Verify that the second file exists. If not, give the user feedback and let them re-enter the second filename.
-                for(int i = 0; i < numFiles; i++){
-                    string prompt = (i + 1).ToString();
-                    filenames.Add(getFileName(prompt));
+                System.Console.WriteLine("Files to merge, blank link when finished. Minimum of 2 files");
+
+                // Run continuously until given a blank return
+                while(true) {
+                    string inputName = getFileName(numFiles);
+                    if(inputName == "") {
+                        break;
+                    }
+
+                    filenames.Add(inputName);
+                    numFiles++;                    
                 }
+
 
                 // Prompt the user for a filename for the new file containing the merged content you will create. In the prompt present them with the default of the merged names if they don’t enter a name (e.g. they choose the default by just hitting enter). Also, if they don’t supply an extension of .txt, append .txt on the end.
                 // Example: Enter new filename (default: File1File2.txt):
@@ -57,8 +68,8 @@ namespace DocumentMerger
                     return;
                 }
                 // If an exception does not occur, output “[filename] was successfully saved. The document contains [count] characters.” and exit. [filename] and [count] are placeholders for the filename of the document and the number of characters it contains.
-                wordCount = getCharacterCount(newFile);
-                displayStats(newFile,wordCount);
+                characterCount = getCharacterCount(newFile);
+                displaySaveMessage(newFile,characterCount);
 
                 // After the program does or does not fail to merge the files, ask the user if they would like to merge two more files. If they do, prompt them again for input. If not, exit the program.
                 if(!doAgain()) {
@@ -66,30 +77,30 @@ namespace DocumentMerger
                 }
             }
 
-            // Console.WriteLine("Press <ENTER> to exit");
-            // Console.ReadLine();
         }
 
+        // Prompt to repeat the process
         private static bool doAgain()
         {
             string input;
-            bool output;
             Console.WriteLine("Would you like to do another merge?");
             input = Console.ReadLine().ToUpper();
+            // Y or YES input, repeat. Otherwise, Dont repeat
             if(input == "Y" || input == "YES" ) {
-                output = true;
+                return true;
             }
             else {
-                output = false;
+                return false;
             }
-            return output;
         }
 
-        private static void displayStats(string file, int count)
+        // Display message about successfully save file and character count
+        private static void displaySaveMessage(string file, int count)
         {
             System.Console.WriteLine($"{ file } was successfully saved. The document contains { count } characters.");
         }
 
+        // Count number of characters (not including spaces) in file and return
         private static int getCharacterCount(string file)
         {
             int output = 0;
@@ -99,9 +110,6 @@ namespace DocumentMerger
             try {
                 StreamReader fileStream = new StreamReader(file);
                 while((line = fileStream.ReadLine()) != null) {
-                    System.Console.WriteLine(line);
-                    
-                    // split on space
                     char[] characters = line.ToCharArray();
                     foreach(char c in characters){
                         if(c.ToString() != " ") {
@@ -117,6 +125,7 @@ namespace DocumentMerger
             return output;
         }
 
+        // Read files in array and write them all to the output file
         private static void readToFile(string outputFile, params string[] files)
         {
             List<string> linesToWrite = new List<string>();
@@ -125,18 +134,18 @@ namespace DocumentMerger
                 foreach(string file in files) {
                     using(StreamReader fileStream = new StreamReader(file)) {
                         linesToWrite.Add(fileStream.ReadToEnd());
+                    }
                 }
-            }
-
                 writeLines(outputFile, linesToWrite);
             }
-            catch (System.Exception)
+            catch (Exception ex)
             {
-                throw;
+                displayException(ex);
             }
 
         }
 
+        // Write all lines from array into file
         private static void writeLines(string file, List<string> lines)
         {
             using(StreamWriter writeStream = new StreamWriter(file)) {
@@ -146,6 +155,7 @@ namespace DocumentMerger
             }
         }
 
+        // Overload for List instead of Array
         private static void readToFile(string outputFile, List<string> files)
         {
             string[] filesArray = files.ToArray();
@@ -153,14 +163,17 @@ namespace DocumentMerger
             readToFile(outputFile, filesArray);
         }
 
+        // Show exception with custom formatting
         private static void displayException(Exception ex) {
             Console.WriteLine("----------------");
             Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.GetType());
             Console.WriteLine("----------------");
             Console.WriteLine(ex.StackTrace);
             System.Console.WriteLine("----------------");
         }
 
+        // Prompt user for name to save new document under. 
         private static string getSaveName(string[] files = null)
         {
             string input, filename, defaultF = "";
@@ -173,15 +186,18 @@ namespace DocumentMerger
                 }
             }
 
-            System.Console.WriteLine($"Enter new filename for merged document. (default: { defaultF })");
+            System.Console.WriteLine($"Enter new filename for merged document. (default: { defaultF }.txt)");
             input = Console.ReadLine();
 
+            // Trim spaces from input
             input = input.Trim();
 
+            // Default to filenames all together
             if(input == "" || input == null) {
                 input = defaultF;
             }
 
+            // Check for .txt ending on filename
             if(!input.EndsWith(".txt")) {
                 filename = input + ".txt";
             }
@@ -198,11 +214,13 @@ namespace DocumentMerger
             return filename;
         }
 
+        // Welcome the user, or rather show a title and be boring
         private static void welcomeUser()
         {
             Console.WriteLine("Document Merger\n");
         }
 
+        // Verify that files exists. 
         private static bool verifyFileExists(string filename)
         {
             bool output = false;
@@ -212,13 +230,21 @@ namespace DocumentMerger
             return output;
         }
 
-        private static string getFileName(string prompt)
+        // Prompt user for filename. Give a file number to insert into prompt. 
+        private static string getFileName(int fileNumber)
         {
             string input,filename = "";
-            Console.WriteLine($"Please enter name for file {prompt}");
+            Console.WriteLine($"Please enter name for file { (fileNumber + 1).ToString() }");
             input = Console.ReadLine();
+            // If user gives a blank/null before we have at least 2 files, yell at them and tell them to do what they are told. 
             if(input == null || input == "") {
-                input = getFileName(prompt);
+                if(fileNumber >= 2) {
+                    return "";
+                }
+                else {
+                    Console.WriteLine("Must have at least 2 files to merge");
+                    input = getFileName(fileNumber);
+                }
             }
 
             if(input.EndsWith(".txt")){
@@ -227,9 +253,10 @@ namespace DocumentMerger
             else {
                 filename = input + ".txt";
             }
-
+            
+            // Yes, the file must exist in order to merge it in. 
             if(!verifyFileExists(filename)) {
-                filename = getFileName(prompt);
+                filename = getFileName(fileNumber);
             }
 
             return filename;
@@ -237,3 +264,5 @@ namespace DocumentMerger
 
     }
 }
+
+// Comments in the program are not great. Most are self explainatory. Sorry. :)
